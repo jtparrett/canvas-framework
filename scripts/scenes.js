27dfabe8d4e77d91551774b0aab3e7c54c689a2b
Josh Parrett <jtparrett@me.com>
2016-02-16 10:04:55 +0000

    scroll work

--- scripts/scenes.js
+++ scripts/scenes.js
@@ -2,6 +2,7 @@ var App = function(){
   var self = this;
   this.canvas = document.getElementById('canvas');
   this.context = this.canvas.getContext('2d');
+  this.body = document.getElementsByTagName('body')[0];
   this.scenes = [];
 
   this.resize();
@@ -9,18 +10,22 @@ var App = function(){
 
   window.onresize = function(){
     self.resize();
+    self.setScrollHeight();
   };
 };
 
 App.prototype.addScene = function(render){
-  this.scenes.push(new Scene(this, render));
+  this.scenes.push(new Scene(this, render, this.scenes.length));
+  this.setScrollHeight();
+};
+
+App.prototype.setScrollHeight = function(){
+  this.body.style.height = this.scenes.length * this.height + 'px';
 };
 
 App.prototype.resize = function(){
-  this.width = window.innerWidth;
-  this.height = window.innerHeight;
-  this.canvas.width = this.width;
-  this.canvas.height = this.height;
+  this.canvas.width = this.width = window.innerWidth;
+  this.canvas.height = this.height = window.innerHeight;
 };
 
 App.prototype.render = function(){
@@ -36,7 +41,8 @@ App.prototype.render = function(){
   });
 };
 
-var Scene = function(app, render){
+var Scene = function(app, render, index){
   this.app = app;
   this.render = render;
+  this.index = index;
 };
--- scripts/scenes/intro.js
+++ scripts/scenes/intro.js
@@ -1,17 +1,17 @@
 var CF = window.CF || {};
 CF.intro = function(){
   var context;
-  var x = 0;
 
   function init(){
     context = CF.app.context;
     CF.app.addScene(render);
+    CF.app.addScene(render);
   }
 
   function render(){
-    x += 2;
+    var offsetTop = - window.scrollY + (CF.app.height * this.index);
     context.fillStyle = '#ff8330';
-    context.fillRect(x, 20, 50, 50);
+    context.fillRect(20, offsetTop + 20, 50, 50);
   }
 
   return {
